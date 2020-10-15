89e2bf15aac770f0d7b5cd688b08945fbaebd583
brookeyoung2 <bayin2003@gmail.com>
2020-10-15 22:39:41 +0000

    Added the last part that I forgot

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -32,6 +32,24 @@ System.out.println(card);
   card.loadMoney(-15);
   System.out.println("Billy: " + card2);
 
+System.out.println();
+TerrapinCard cardBilly= new TerrapinCard (20);
+TerrapinCard cardBrian = new TerrapinCard (30);
+cardBilly.payGourmet();
+cardBrian.payEconomical();
+System.out.println("Billy: The card has " + cardBilly.getBalance() + " dollars");
+System.out.println("Brian: The card has " + cardBrian.getBalance() + " dollars");
+cardBilly.loadMoney(20);
+cardBrian.payGourmet();
+System.out.println("Billy: The card has " + cardBilly.getBalance() + " dollars");
+System.out.println("Brian: The card has " + cardBrian.getBalance() + " dollars");
+cardBilly.payEconomical();
+cardBilly.payEconomical();
+cardBrian.loadMoney(50);
+System.out.println("Billy: The card has " + cardBilly.getBalance() + " dollars");
+System.out.println("Brian: The card has " + cardBrian.getBalance() + " dollars");
+
+
 
 
 
